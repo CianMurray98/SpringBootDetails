400217351345bbd21a0f8c9c83e810f2dba36a11
CianMurray98 <cianmurraycian@gmail.com>
2023-10-06 15:07:29 +0100

    homework complete but not working

---
+++ src/main/java/ie/atu/springbootdetails/Calculation.java
@@ -0,0 +1,52 @@
+package ie.atu.springbootdetails;
+
+public class Calculation {
+    //int num1 = 6, num2 = 2, num3 = 10, num4 = 5, num5 = 4, num6 = 3, num7 = 8, num8 = 2;
+    double num1, num2;
+    double add = num1 + num2;
+    double sub = num1 - num2;
+    double multiply = num1 * num2;
+    double divide = num1 / num2;
+
+    public double total;
+    public double Calculation;
+    //public double getTotal() {
+
+    //    return total;
+    //}
+    //public void setTotal(double Total){
+    //    this.Calculation = total;
+    //}
+
+    public double getAdd() {
+        return total;
+    }
+
+    public void setAdd(double add) {
+        this.add = add;
+    }
+
+    public double getSub() {
+        return total;
+    }
+
+    public void setSub(double sub) {
+        this.sub = sub;
+    }
+
+    public double getMultiply() {
+        return total;
+    }
+
+    public void setMultiply(double multiply) {
+        this.multiply = multiply;
+    }
+
+    public double getDivide() {
+        return total;
+    }
+
+    public void setDivide(double divide) {
+        this.divide = divide;
+    }
+}
--- src/main/java/ie/atu/springbootdetails/CalculatorController.java
+++ src/main/java/ie/atu/springbootdetails/CalculatorController.java
@@ -1,4 +1,35 @@
 package ie.atu.springbootdetails;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CalculatorController {
+    double total;
+
+    Calculation values = new Calculation();
+    @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public String calculate(@PathVariable double num1, @PathVariable double num2, @PathVariable String operation){
+        return "operation: " + operation + " total: " + total + values.getSub();
+    }
 }
+/*
+    double result;
+        switch (operation.toLowerCase()){
+                case "add":
+                result = num1 + num2;
+                break;
+                case "subtract":
+                result = num1 - num2;
+                break;
+                case "multiply":
+                result = num1 * num2;
+                break;
+                case "divide":
+                result = num1 / num2;
+
+default:
+        throw new IllegalArgumentException("Invalid Operation: " + operation);
+        }*/
